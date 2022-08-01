1a403cf498ec9c7c58d81f7191ea0ed850c3a4c6
alirezaSafi <alireza.safi.099@gmail.com>
2022-08-01 22:54:36 +0000

    Add liveness and readiness probes for nrf

--- oai_vnf/juju-bundles/charms/oai-nrf/lib/pod_spec.py
+++ oai_vnf/juju-bundles/charms/oai-nrf/lib/pod_spec.py
@@ -21,9 +21,51 @@ def make_pod_ports(config: Dict[str, Any]) -> List[Dict[str, Any]]:
     ]
 
 
+def live_ready_script() -> str:
+    with open("scipts/live-ready.sh") as text_file:
+        return text_file.read()
+
+
+def make_volume_config() -> List[Dict[str, Any]]:
+    return [
+        {
+            "name": "scripts",
+            "mountPath": "/scripts",
+            "files": [
+                {
+                    "path": "live-ready.sh",
+                    "content": live_ready_script()
+                }
+            ]
+        }
+    ]
+
+
+def make_liveness_probe() -> Dict[str, Any]:
+    return {
+        "exec": {
+            "command": ["sh", "/scripts/live-ready.sh"]
+        },
+        "initialDelaySeconds": 15,
+        "periodSeconds": 5
+    }
+
+
+def make_readiness_probe() -> Dict[str, Any]:
+    return {
+        "exec": {
+            "command": ["sh", "/scripts/live-ready.sh"]
+        },
+        "initialDelaySeconds": 5,
+        "periodSeconds": 5
+    }
+
 def make_pod_spec(config: Dict[str, Any]) -> Dict[str, Any]:
     """make pod spec details"""
     ports = make_pod_ports(config)
+    volume_config = make_volume_config()
+    liveness_probe = make_liveness_probe()
+    readiness_probe = make_readiness_probe()
     return {
         "version": 3,
         "containers": [
@@ -39,6 +81,11 @@ def make_pod_spec(config: Dict[str, Any]) -> Dict[str, Any]:
                     "NRF_INTERFACE_PORT_FOR_SBI": config["nrf-interface-port-for-sbi"],
                     "NRF_INTERFACE_HTTP2_PORT_FOR_SBI": config["nrf-interface-http2-port-for-sbi"],
                     "NRF_API_VERSION": config["nrf-api-version"]
+                },
+                "volumeConfig": volume_config,
+                "kubernetes": {
+                    "livenessProbe": liveness_probe,
+                    "readinessProbe": readiness_probe
                 }
             }
         ]
