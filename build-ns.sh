5247ae74f31bf4673de3ed953455b446023e4a20
alirezaSafi <alireza.safi.099@gmail.com>
2022-11-14 10:43:50 +0000

    Update params data

--- build-ns.sh
+++ build-ns.sh
@@ -2,4 +2,4 @@
 
 osm nfpkg-create oai_vnf
 osm nspkg-create oai_ns
-osm ns-create --ns_name oai-ns --nsd_name oai-ns --vim_account hackfest-dummy
+osm ns-create --ns_name oai-ns --nsd_name oai-ns --vim_account hackfest-dummy --config_file params.yaml
